4e3c1d94de102adaec5e21304dd402bf2b95863c
Niko <soder.n@gmail.com>
2024-07-13 15:15:42 +0300

    style: add button to PlaylistItem

--- client/src/Playlist.tsx
+++ client/src/Playlist.tsx
@@ -46,25 +46,29 @@ export function PlaylistItem({ playlist }: { playlist: TPLaylist }) {
     socket.emit("playlist", playlistId);
   }
   return (
-    <li
-      onClick={() => connect(playlist.id)}
-      className="flex p-3 rounded-md cursor-pointer hover:bg-stone-900"
-    >
-      {playlist.image && (
-        <div className="me-4">
-          <img
-            className="rounded-md w-full max-w-[60px] h-auto"
-            src={playlist.image}
-            alt="playlist image"
-          />
+    <li>
+      <button
+        onClick={() => connect(playlist.id)}
+        className="w-full flex p-3 rounded-md cursor-pointer hover:bg-stone-900"
+      >
+        {playlist.image && (
+          <div className="me-4">
+            <img
+              className="rounded-md w-full max-w-[60px] h-auto"
+              src={playlist.image}
+              alt="playlist image"
+            />
+          </div>
+        )}
+        <div className="*:text-left">
+          <p className="text-white font-bold">{playlist.name}</p>
+          <div>
+            <span className="text-stone-400">{playlist.type}</span>
+            <span className="text-stone-400">&#x2B1D;</span>
+            <span className="text-stone-400">{playlist.owner}</span>
+          </div>
         </div>
-      )}
-      <div>
-        <p className="text-white font-bold">{playlist.name}</p>
-        <span className="text-stone-400">{playlist.type}</span>
-        <span className="text-stone-400">&#x2B1D;</span>
-        <span className="text-stone-400">{playlist.owner}</span>
-      </div>
+      </button>
     </li>
   );
 }
