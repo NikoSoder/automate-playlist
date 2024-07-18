76bc4859b42ebe742175b6a5ee4203fde0ea7c51
Niko <soder.n@gmail.com>
2024-07-18 18:59:08 +0300

    feat: add now listening song types to client

--- client/src/types.ts
+++ client/src/types.ts
@@ -11,3 +11,16 @@ export interface TPLaylist {
 type ResultSuccess<T> = { success: true; value: T };
 type ResultError<E> = { success: false; error: E };
 export type Result<T, E> = ResultSuccess<T> | ResultError<E>;
+
+export interface Song {
+  uri: string;
+  images: Image[];
+  trackName: string;
+  artistNames: string[];
+}
+
+export interface Image {
+  url: string;
+  height: number;
+  width: number;
+}
