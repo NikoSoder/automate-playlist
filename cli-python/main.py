e7ae7f68d3e6534f194d3345936d1d894787135a
NikoSoder <105541058+NikoSoder@users.noreply.github.com>
2024-07-28 09:08:11 +0300

    Playlist id (#3)
    * feat: get playlist id from command line argument
    
    * refactor: error handling

--- cli-python/main.py
+++ cli-python/main.py
@@ -5,10 +5,15 @@ from dotenv import load_dotenv
 import os
 import time
 import sys
+import argparse
+
+# get playlist id
+parser = argparse.ArgumentParser(description="Spotify playlist id")
+parser.add_argument("spotify_playlist_id", type=str, help="Playlist id")
+args = parser.parse_args()
+PLAYLIST_ID = args.spotify_playlist_id
 
 load_dotenv()
-# TODO: how to get playlist id
-TEST_PLAYLIST_ID = "3NDunacCS4ElOReYC6sEHH"
 CHECK_CURRENTLY_PLAYING_TRACK_WAIT_TIME = 90  # 90s
 previous_songs = []  # song uris
 client_id = os.getenv("CLIENT_ID")
@@ -25,13 +30,25 @@ sp = spotipy.Spotify(
     )
 )
 
-# get playlist tracks to prevent dublicates
-playlist_tracks_info = sp.playlist_tracks(TEST_PLAYLIST_ID)
-if playlist_tracks_info:
-    playlist_tracks = playlist_tracks_info["items"]
-    for track in playlist_tracks:
-        uri = track["track"]["uri"]
-        previous_songs.append(uri)
+try:
+    # get playlist tracks to prevent dublicates
+    playlist_tracks_info = sp.playlist_tracks(PLAYLIST_ID)
+    if playlist_tracks_info:
+        playlist_tracks = playlist_tracks_info["items"]
+        for track in playlist_tracks:
+            uri = track["track"]["uri"]
+            previous_songs.append(uri)
+
+except spotipy.SpotifyException as e:
+    print(f"An error occurred: {e}")
+    print(f"Exception type: {e.__class__.__name__}")
+    print(f"Exception args: {e.args}")
+    sys.exit()
+except Exception as e:
+    print(f"An error occurred: {e}")
+    print(f"Exception type: {e.__class__.__name__}")
+    print(f"Exception args: {e.args}")
+    sys.exit()
 
 while True:
     # get currently playing track
@@ -49,7 +66,7 @@ while True:
 
     print(artist_names)
     # print(song_uri)
-    # print(song_name)
+    print(song_name)
     # print(song_artists)
 
     # add song to a selected playlist
@@ -57,7 +74,7 @@ while True:
         print("adding song to playlist...")
         print(song_uri)
         # TODO: error handling
-        snapshot_id = sp.playlist_add_items(TEST_PLAYLIST_ID, [song_uri])
+        snapshot_id = sp.playlist_add_items(PLAYLIST_ID, [song_uri])
         print(snapshot_id)
         previous_songs.append(song_uri)
 
