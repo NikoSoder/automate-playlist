9e6ee283adbd77455091ff2096bafdb8289f23f0
Niko <soder.n@gmail.com>
2024-07-29 19:12:29 +0300

    feat: center title

--- cli-python/border.py
+++ cli-python/border.py
@@ -5,9 +5,7 @@ def print_border(name, artists):
     cols = max(len(name), len(merged_artists), len(title)) + PADDING_RIGHT
     # borders around song
     print()
-    print_corners(cols)
-    print_emptlyline_with_border(cols)
-    print_word(title, cols)
+    print_title_middle(title, cols)
     print_emptlyline_with_border(cols)
     print_word(name, cols)
     print_word(merged_artists, cols)
@@ -16,14 +14,22 @@ def print_border(name, artists):
     print()
 
 
+def print_title_middle(title, cols):
+    # NOTE: a lot of magic numbers...
+    title_length = len(title)
+    padding = round((cols - title_length) / 2) + 1
+    extra_padding = (cols - title_length) % 2
+    print("+" + "-" * (padding) + title + "-" * (padding + extra_padding) + "+")
+
+
 def print_word(word, cols):
     word_length = len(word)
-    print(f"| {word}{' ' * (cols - word_length - 2)} |")
+    print(f"| {word}{' ' * (cols - word_length)} |")
 
 
 def print_corners(cols):
-    print("+" + "-" * (cols) + "+")
+    print("+" + "-" * (cols) + "-" + "-" + "+")
 
 
 def print_emptlyline_with_border(cols):
-    print(f"|{' ' * (cols)}|")
+    print(f"| {' ' * (cols)} |")
