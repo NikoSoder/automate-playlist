4420f27851798ed931cca5d33d2c5519716028bc
Niko <soder.n@gmail.com>
2024-08-10 11:45:50 +0300

    feat: playlist selection
    change commandline argument of playlist to list of playlist
    where user can select which playlist currently playing songs will go.

--- main.py
+++ main.py
@@ -5,20 +5,13 @@ from dotenv import load_dotenv
 from os import system, name, getenv
 import time
 import sys
-import argparse
 from border import print_border
 from requests.exceptions import ReadTimeout
-from typing import Callable
-
-# get playlist id
-parser = argparse.ArgumentParser(description="Spotify playlist id")
-parser.add_argument("spotify_playlist_id", type=str, help="Playlist id")
-args = parser.parse_args()
-PLAYLIST_ID = args.spotify_playlist_id
+from typing import Callable, List, Optional
 
 load_dotenv()
 CHECK_CURRENTLY_PLAYING_TRACK_WAIT_TIME = 60  # 60s
-previous_songs = []  # song uris
+previous_songs = []  # song uris TODO: move this to Playlist class
 client_id = getenv("CLIENT_ID")
 client_secret = getenv("CLIENT_SECRET")
 redirect_uri = getenv("REDIRECT_URI")
@@ -34,6 +27,85 @@ sp = spotipy.Spotify(
         # retries=10
     )
 )
+
+
+def handle_api_call(func: Callable, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except ReadTimeout as e:
+        print("\a")  # NOTE: remove this later(bell sound for debug)
+        print(f"ReadTimeout occurred: {e}")
+        return func(*args, **kwargs)
+    except spotipy.SpotifyException as e:
+        print(f"SpotifyException: {e}")
+        sys.exit()
+    except ConnectionError as e:
+        print(f"ConnectionError: {e}")
+        sys.exit()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        sys.exit()
+
+
+def clear_terminal():
+    # for windows
+    if name == "nt":
+        system("cls")
+        return
+
+    # for mac, linux
+    system("clear")
+
+
+class Playlist:
+    def __init__(self, id: str, name: str):
+        self.id = id
+        self.name = name
+        """
+        save playlist songs here aswell?
+        now playlist songs are in 'previous_songs' 
+        """
+
+
+def user_select_playlist() -> Optional[Playlist]:
+    clear_terminal()
+    user_playlists: List[Playlist] = []
+    playlists = handle_api_call(sp.current_user_playlists, 20)
+    if not playlists:
+        sys.exit("No playlists found")
+
+    for idx, playlist in enumerate(playlists["items"]):
+        name = playlist["name"]
+        id = playlist["id"]
+        user_playlists.append(Playlist(id, name))
+        print(f"({idx + 1}), {name}")
+
+    print()
+
+    try:
+        user_selected_playlist_idx = int(
+            input("Select a playlist to save the currently playing songs: ")
+        )
+
+        if user_selected_playlist_idx < 1 or user_selected_playlist_idx > len(
+            user_playlists
+        ):
+            print("Not a valid index")
+            return None
+
+        selected_playlist = user_playlists[user_selected_playlist_idx - 1]
+        return selected_playlist
+    except ValueError as e:
+        print(f"Not a valid number: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    return None
+
+
+PLAYLIST_ACTIVE = user_select_playlist()
+if not PLAYLIST_ACTIVE:
+    sys.exit()
 
 
 def animated_music_icon(duration):
@@ -69,35 +141,7 @@ def animated_music_icon(duration):
     sys.stdout.flush()
 
 
-def handle_api_call(func: Callable, *args, **kwargs):
-    try:
-        return func(*args, **kwargs)
-    except ReadTimeout as e:
-        print("\a")  # NOTE: remove this later(bell sound for debug)
-        print(f"ReadTimeout occurred: {e}")
-        return func(*args, **kwargs)
-    except spotipy.SpotifyException as e:
-        print(f"SpotifyException: {e}")
-        sys.exit()
-    except ConnectionError as e:
-        print(f"ConnectionError: {e}")
-        sys.exit()
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        sys.exit()
-
-
-def clear_terminal():
-    # for windows
-    if name == "nt":
-        system("cls")
-        return
-
-    # for mac, linux
-    system("clear")
-
-
-playlist_tracks_info = handle_api_call(sp.playlist_tracks, PLAYLIST_ID)
+playlist_tracks_info = handle_api_call(sp.playlist_tracks, PLAYLIST_ACTIVE.id)
 if playlist_tracks_info:
     playlist_tracks = playlist_tracks_info["items"]
     for track in playlist_tracks:
@@ -106,7 +150,7 @@ if playlist_tracks_info:
 
 
 while True:
-    # clear_terminal()
+    clear_terminal()
     # get currently playing track
     song = handle_api_call(sp.current_user_playing_track)
     if not song:
@@ -126,8 +170,8 @@ while True:
 
     # add song to a selected playlist
     if song_uri not in previous_songs:
-        handle_api_call(sp.playlist_add_items, PLAYLIST_ID, [song_uri])
-        print(f"adding {song_name} to a playlist...")
+        handle_api_call(sp.playlist_add_items, PLAYLIST_ACTIVE.id, [song_uri])
+        print(f"Adding song: {song_name} to the playlist: {PLAYLIST_ACTIVE.name}")
         previous_songs.append(song_uri)
 
     animated_music_icon(CHECK_CURRENTLY_PLAYING_TRACK_WAIT_TIME)
